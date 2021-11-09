5097a9356f223e27554b2e3aef97e4b8a4615292
cka-dev <cka@cka.dev>
2021-11-09 17:51:52 -0600

    Refactors FCM registration service from FirebaseInstanceIdService to FirebaseMessagingService
    Details:
    This is similar to http://legit/187091 and is an oversight from http://legit/184711
    I intended to submit it afterwards but left it out.
    AndroidManifest.xml has lasready been updated to account for this change
    
    Bug: 193796318
    Change-Id: I022d25357ede6bf69668352b268a21cb29a2eb45

---
+++ ClassyTaxiJava/app/src/main/java/com/sample/android/classytaxijava/FcmRegistrationTokenService.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2020 Google LLC. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.sample.android.classytaxijava;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+/**
+ * Gets a FCM registration token and sends it to Server for validation and registration.
+ */
+public class FcmRegistrationTokenService extends FirebaseMessagingService {
+    /**
+     * Called if registration token is updated. This may occur if the security of
+     * the previous token had been compromised. Note that this is called when the token
+     * is initially generated so this is where you would retrieve the token.
+     */
+    @Override
+    public void onNewToken(@NonNull String token) {
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // FCM registration token to your app server.
+        // Persists it to the server.
+        ((SubApp) getApplication()).getRepository().registerInstanceId(token);
+
+    }
+}
